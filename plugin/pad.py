805c986ba3b0f3452b9464cb10b137969547dc02
Felipe Morales <hel.sheep@gmail.com>
2012-01-05 16:49:51 -0200

    Now, for good.

--- plugin/pad.py
+++ plugin/pad.py
@@ -109,9 +109,9 @@ class Pad(object):
 				vim.command("set listchars+=precedes:«")
 		else:
 			if "extends" not in tmp:
-				vim.command("set listchars+=extends:<bb>")
+				vim.command("set listchars+=extends:»")
 			if "precedes" not in tmp:
-				vim.command("set listchars+=precedes:<ab>")
+				vim.command("set listchars+=precedes:«")
 		del tmp
 	
 
